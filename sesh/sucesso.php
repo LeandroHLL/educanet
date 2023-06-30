317f0d02fd48611fe98b9b775e5a7ace5ac86275
LeandroHll <leandrohll@live.com>
2023-06-30 16:21:44 -0300

    tratando erros

--- sesh/sucesso.php
+++ sesh/sucesso.php
@@ -110,8 +110,9 @@ if (isset($_SESSION['username'])) {
                             </h6>
                             <h6>Curso Escolhido:<p><?php echo $selectedCursoNome; ?>!</p>
                             </h6>
-                            <h6>Sua Senha:<p><?php echo $autenticacao; ?>!</p>
+                            <h6>Sua Senha:<p><?php echo (!empty($autenticacao) ? $autenticacao : 'Nenhuma senha disponível no momento'); ?>!</p>
                             </h6>
+
                         </div>
                         <a href="logout.php">Sair</a> <!-- Botão de logout -->
                     </div>
